c5eada4acd262a785dc94d469b3dd817361c984a
Bhavya Bharti <bhavyab@outlook.com>
2017-08-02 00:02:53 -0700

    response header

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use('./index.html');
+
+app.use(express.static(`${__dirname}`));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
