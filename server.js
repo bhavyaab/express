2d224ba7599c7802536ad369501fa8cc2f24a648
Bhavya Bharti <bhavyab@outlook.com>
2017-08-01 23:57:08 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use('./index.html');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.post('/contact', function(req, res){
+  res.send('post success');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
