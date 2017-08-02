7bd55abdddd4cd211dc6584b22770f88b2d1dafc
Bhavya Bharti <bhavyab@outlook.com>
2017-08-02 00:20:41 -0700

    build folder added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
 });
 
 
-app.use(express.static(`${__dirname}`));
+app.use(express.static(`${__dirname}/build`));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
